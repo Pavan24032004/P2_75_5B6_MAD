4adc0db57252008acb783df384c353125b900965
pavan <123@>
2023-08-04 11:58:54 +0530

    a

---
+++ p3_75_5B6/app/src/main/java/com/pavanpatel/p3_75_5b6/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.p3_75_5b6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pavanpatel.p3_75_5b6.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
